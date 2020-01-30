8c9e55b3c27ad989388537c9c3efb48d846ee18f
anjan93 <49583590+anjan93@users.noreply.github.com>
2020-01-30 10:43:40 -0800

    Add files via upload

--- modeling.m
+++ modeling.m
@@ -1,7 +1,7 @@
-time=[0:10:200];
+time=[0:10:300];
 const1=exp(-1*0.0453*time)-exp(-1*0.0224*time);
 const2=0.0453/(0.0224-0.0453);
-const3=100*const1*const2;
+const3=73*const1*const2;
 Gzero=90;
 Gt=const3+Gzero;
 plot(time,Gt);
